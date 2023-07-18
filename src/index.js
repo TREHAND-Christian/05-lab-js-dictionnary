fcc87f0f208547121fb268f62955d561920b209c
Christian TREHAND <126438065+TREHAND-Christian@users.noreply.github.com>
2023-07-18 06:07:55 +0200

    changement bouton theme

--- src/index.js
+++ src/index.js
@@ -1,90 +1,91 @@
-import './scss/style.scss';
-import getAPIResponse from './api-dico.js';
+import "./scss/style.scss";
+import getAPIResponse from "./api-dico.js";
 
-const form = document.querySelector('form');
-const input = document.getElementById('myInput');
-const theme = document.querySelector('body');
-const logo = document.querySelector('#logo');
-const definition = document.getElementById('def');
-
+const form = document.querySelector("form");
+const input = document.getElementById("myInput");
+const theme = document.querySelector("body");
+const definition = document.getElementById("def");
 
 let isDark = false;
 
 // Fonction changement de thème
 function swapTheme() {
-    const lien = document.querySelectorAll('.lien');
+    const lien = document.querySelectorAll(".lien");
     const element = document.body;
     const styles = getComputedStyle(element);
     const coulTxt = styles.color;
     const coulBg = styles.backgroundColor;
+    const switchThemeImg = document.getElementById("switch-theme");
+
     if (isDark) {
         theme.style.background = coulTxt;
         theme.style.color = coulBg;
         logoImg.src = "/src/img/logo-dark.png";
         for (let i = 0; i < lien.length; i++) {
-            lien[i].classList.add('inverted');
-          }
+            lien[i].classList.add("inverted");
+        }
+        switchThemeImg.src = "/src/img/bt-light.png";
     } else {
         theme.style.background = coulTxt;
         theme.style.color = coulBg;
         logoImg.src = "/src/img/logo.png";
         for (let i = 0; i < lien.length; i++) {
-            lien[i].classList.remove('inverted');
-          }
+            lien[i].classList.remove("inverted");
+        }
+        switchThemeImg.src = "/src/img/bt-dark.png";
     }
 }
 
+// Événement changement de thème
+document.getElementById("switch-theme").addEventListener("click", () => {
+    isDark = !isDark; // Inverse la valeur de isDark
+    swapTheme();
+});
+
 // Événement Submit
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     const word = input.value;
-    getAPIResponse(word, 'en');
+    getAPIResponse(word, "en");
 });
 
 // Événement input focus
-input.addEventListener('focus', () => {
+input.addEventListener("focus", () => {
     const erreur = document.querySelector("#erreur");
     erreur.textContent = "";
 });
 
-// Événement changement de thème
-document.getElementById('lightTheme').addEventListener('click', () => {
-    isDark = false;
-    swapTheme();
-});
-
-document.getElementById('darkTheme').addEventListener('click', () => {
-    isDark = true;
-    swapTheme();
-});
-
 // Événement Lien Synonymes / Antonymes
-document.addEventListener('click', (event) => {
-    const {target} = event;
-    if (target.tagName === 'A' && target.className === 'lien') {
+document.addEventListener("click", (event) => {
+    const {
+        target
+    } = event;
+    if (target.tagName === "A" && target.className === "lien") {
         event.preventDefault();
         const content = target.textContent;
-        getAPIResponse(content, 'en');
+        getAPIResponse(content, "en");
         input.value = content;
     }
 });
 
 // Événement changement de police
-document.querySelectorAll('.switch-toggle input[type="radio"]').forEach((input) => {
-    input.addEventListener('change', (e) => {
-        switch (e.target.id) {
-            case 'sanssherif':
-                definition.style.fontFamily = 'sans-serif';
-                break;
-            case 'sherif':
-                definition.style.fontFamily = 'serif';
-                break;
-            case 'monospace':
-                definition.style.fontFamily = 'monospace';
-                break;
-        }
+document
+    .querySelectorAll('.switch-toggle input[type="radio"]')
+    .forEach((input) => {
+        input.addEventListener("change", (e) => {
+            switch (e.target.id) {
+                case "sanssherif":
+                    definition.style.fontFamily = "sans-serif";
+                    break;
+                case "sherif":
+                    definition.style.fontFamily = "serif";
+                    break;
+                case "monospace":
+                    definition.style.fontFamily = "monospace";
+                    break;
+            }
+        });
     });
-});
 
 // Initialisation du "mot" par défaut
-getAPIResponse('word', 'en');
+getAPIResponse("word", "en");
